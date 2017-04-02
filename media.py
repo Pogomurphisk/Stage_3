5b464d78ded09283c7cb9df002490901f8ea3780
Pogomurphisk <claycreger@gmail.com>
2017-04-02 13:50:57 -0700

    Format
    Formatted documentation

--- media.py
+++ media.py
@@ -2,15 +2,18 @@ import webbrowser
 
 class Movie():
 	"""A class that contains methods and information on movies"""
+
 	def __init__(self, movie_title, poster_image,
 				 trailer_youtube):
 		"""Initialize instance of movie class using movie_title, which is a
 		string of the movie title, poster_image, a url of the movie poster,
 		and trailer_youtube, a link to the movie trailer on youtube."""
+
 		self.title = movie_title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
 	def show_trailer(self):
 		"""Open an instance of this movie's trailer in youtube"""
+		
 		webbrowseropen(self.trailer_youtube_url)
