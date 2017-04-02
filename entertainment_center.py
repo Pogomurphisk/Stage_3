83fc853d7d3a22275ea525c69967c9e279f3bdec
Pogomurphisk <claycreger@gmail.com>
2017-04-01 23:39:23 -0700

    Syntax
    Changed syntax to conform to standards

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,23 @@
 import media
 import fresh_tomatoes
 
-con_air = media.Movie("Con Air", 
+con_air = media.Movie(
+	"Con Air", 
 	"https://upload.wikimedia.org/wikipedia/en/1/1d/Conairinternational.jpg",
 	"https://www.youtube.com/watch?v=fWq-S1_1vnc")
 
-the_rock = media.Movie("The Rock",
+the_rock = media.Movie(
+	"The Rock",
 	"https://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_%28movie%29.jpg",
 	"https://www.youtube.com/watch?v=UWfXyNfVotg")
 
-face_off = media.Movie("Face/Off",
+face_off = media.Movie(
+	"Face/Off",
 	"https://upload.wikimedia.org/wikipedia/en/0/0c/FaceOff_%281997_film%29_poster.jpg",
 	"https://www.youtube.com/watch?v=3zebJ_NoduE")
 
-kick_ass = media.Movie("Kick-Ass",
+kick_ass = media.Movie(
+	"Kick-Ass",
 	"https://upload.wikimedia.org/wikipedia/en/3/30/Kick-Ass_film_poster.jpg",
 	"https://www.youtube.com/watch?v=rFpWpkxsVI8")
 
